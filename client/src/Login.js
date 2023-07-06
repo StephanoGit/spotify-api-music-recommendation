2fefdb036d1dedc7fd24748d28b4543816a0df41
Radu-Stefan Popovici <radu.popovici-2@student.manchester.ac.uk>
2023-07-06 17:46:59 +0300

    Improve the layout of the profile page
    Issue #5

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,41 +3,46 @@ import React, { useEffect, useState } from "react";
 import "./style/Login.css";
 
 export default function Login() {
-    const [loginEndPoint, setLoginEndPoint] = useState("");
+  const [loginEndPoint, setLoginEndPoint] = useState("");
 
-	useEffect(() => {
-        axios.post("/login")
-            .then((res) => {
-                console.log(res.data);
-                setLoginEndPoint(res.data);
-            })
-    }, []);
+  useEffect(() => {
+    axios.post("/login").then((res) => {
+      console.log(res.data);
+      setLoginEndPoint(res.data);
+    });
+  }, []);
 
-	return (
-		<div>
-			<div className="welcome-div">
-				<div className="banner-text-fake"><h1>FIND YOUR NEW FAVOURITE MUSIC</h1></div>
-				<div className="welcome-text">
-					<h1>FIND YOUR NEW</h1>
-					<h1>FAVOURITE MUSIC</h1>
-				</div>
-				<div className="welcome-separator"></div>
+  return (
+    <div>
+      <div className="welcome-div">
+        <div className="banner-text-fake">
+          <h1>FIND YOUR NEW FAVOURITE MUSIC</h1>
+        </div>
+        <div className="welcome-text">
+          <h1>FIND YOUR NEW</h1>
+          <h1>FAVOURITE MUSIC</h1>
+        </div>
+        <div className="welcome-separator"></div>
 
-				<div className="login-div">
-					<div className="flex-column">
-					<img
-						src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
-						width={"50%"}></img>
-					<h1>//Web API</h1></div>
-					<div className="d-flex align-items-center"><a
-					className="btn btn-success btn-lg"
-					href={loginEndPoint}
-					style={{ marginTop: "26px" }}
-					>
-					<p className="bttn-txt">LOGIN</p>
-					</a></div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="login-div">
+          <div className="flex-column">
+            <img
+              src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
+              width={"50%"}
+            ></img>
+            <h1>//Web API</h1>
+          </div>
+          <div className="d-flex align-items-center">
+            <a
+              className="btn btn-success btn-lg"
+              href={loginEndPoint}
+              style={{ marginTop: "26px" }}
+            >
+              <p className="bttn-txt">LOGIN</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
-import {Link} from "react-router-dom"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "./style/Profile.css";
 
 export default function Profile({spotifyApi}) {
     const [profile, setProfile] = useState({
@@ -34,7 +35,7 @@ export default function Profile({spotifyApi}) {
                 email: res.email,
                 product: res.product,
                 url: res.external_urls.spotify,
-                photo: res.images[0].url
+                photo: res.images[1].url
             }
             )
         })
@@ -94,98 +95,68 @@ useEffect(() => {
 console.log(topTracks);
 
   return (
-  <div className="profile-info" style={{display:"flex", width:"90vw", margin:"auto", marginTop:"50px", gap:"5vw"}}>
-      <div className="profile-details" style={{display:"flex", flexDirection:"column", alignItems:"center",width:"30vw"}}>
-        <img src={profile.photo} alt="profile" style={{width:"200px", height:"200px", objectFit:"cover", borderRadius:"100px"}}/>
-        <p>{profile.name}</p>
-        <p>{profile.email}</p>
-        <p>{profile.product}</p>
-        <a href={profile.url}>Spotify Profile</a>
+    <div className='profile' style={{display:"flex", flexDirection:"column", width:"90vw", margin:"auto", marginTop:"50px", gap:"5vw"}}>
+      <div className='profile-about'>
+        <h1>//PROFILE</h1>
+        <div className='d-flex'>
+          <img src={profile.photo} alt="profile" style={{width:"200px", height:"200px", objectFit:"cover"}}/>
+          <div className='flex-column'>
+            <p>{profile.name}</p>
+            <p>{profile.email}</p>
+            <p>{profile.product}</p>
+            <a href={profile.url}>Spotify Profile</a>
+          </div>
+        </div>
       </div>
-      <div className="profile-tops">
-        <h2>Top Tracks</h2>
-          <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
+      <div className="separator"></div>
+
+      <div className='profile-fav-tracks'>
+        <h1>//FAVOURITE TRACKS</h1>
+        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
           {topTracks.map((track) => (
-            <div
-            className="card"
-            style={{
-              cursor: "pointer",
-              width: "220px",
-              marginBottom: "30px",
-            }}
-            key={track.id}
-          >
-            <img
-              className="card-img-top"
-              src={track.image}
-              alt={track.name}
-              onMouseOver={() => start(track.preview_url)}
-              onMouseOut={() => stop()}
-            />
-            <div className="card-body">
-              <div>{track.name}</div>
+          <div className='track-card' style={{cursor: "pointer", width: "260px", marginBottom: "30px", padding: "1rem 1rem 0.5rem 1rem", backgroundColor: "#262626"}} key={track.id}>
+            <img className='track-card-img' style= {{width: "100%"}}src={track.image} alt={track.name} onMouseOver={() => start(track.preview_url)} onMouseOut={() => stop()}/>
+            <div className='track-card-body'>
+              <div style={{fontSize:"1rem", color: "white", marginTop: ".5rem"}}>{track.name}</div>
               <div className="text-muted">{track.artist}</div>
-              {track.preview_url == null ? (<div style={{color:"red"}}>No preview available -- Use Search Track</div>) : <div/>}
+              {track.preview_url == null ? (<div style={{color:"red"}}>No preview available</div>) : (<div style={{color:"#262626"}}>Preview available</div>)}
             </div>
           </div>
           ))}
           </div>
-
-          <h2>Top Artists</h2>
-          <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
-          {topArtists.map((artist) => (
-            <div
-            className="card"
-            style={{
-              cursor: "pointer",
-              width: "220px",
-              marginBottom: "30px",
-            }}
-            key={artist.id}
-          >
-            <a href={artist.url} target="_blank" rel="noreferrer">
-              <img
-              className="card-img-top"
-              src={artist.image}
-              alt={artist.name}
-              />
-            </a>
-            <div className="card-body">
-              <div>{artist.name}</div>
-              <div className="text-muted">{artist.genres}</div>
-            </div>
-            </div>
-          ))}
-          </div>
-
-          <h2>Followed Artists</h2>
-          <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
-          {followedArtists.map((artist) => (
-            <div
-            className="card"
-            style={{
-              cursor: "pointer",
-              width: "220px",
-              marginBottom: "30px",
-            }}
-            key={artist.id}
-          >
-            <a href={artist.url} target="_blank" rel="noreferrer">
-              <img
-              className="card-img-top"
-              src={artist.image}
-              alt={artist.name}
-              />
-            </a>
-            <div className="card-body">
-              <div>{artist.name}</div>
-              <div className="text-muted">{artist.genres}</div>
-            </div>
-            </div>
-          ))}
-          </div>
-
       </div>
-    </div>
+
+      <div className="separator"></div>
+      <div className='profile-top-artists'>
+        <h1>//FAVOURITE ARTISTS</h1>
+        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
+          {topArtists.map((artist) => (
+          <div className='track-card' style={{cursor: "pointer", width: "260px", marginBottom: "30px", padding: "1rem 1rem 0.5rem 1rem", backgroundColor: "#262626"}} key={artist.id}>
+            <img className='track-card-img' style= {{width: "100%"}}src={artist.image} alt={artist.name}/>
+            <div className='track-card-body'>
+              <div style={{fontSize:"1rem", color: "white", marginTop: ".5rem"}}>{artist.name}</div>
+              <div className="text-muted">{artist.genres}</div>
+            </div>
+          </div>
+          ))}
+          </div>
+      </div>
+
+      <div className="separator"></div>
+      <div className='profile-followed-artists'>
+        <h1>//FOLLOWED ARTISTS</h1>
+        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
+          {followedArtists.map((artist) => (
+          <div className='track-card' style={{cursor: "pointer", width: "260px", marginBottom: "30px", padding: "1rem 1rem 0.5rem 1rem", backgroundColor: "#262626"}} key={artist.id}>
+            <img className='track-card-img' style= {{width: "100%"}}src={artist.image} alt={artist.name}/>
+            <div className='track-card-body'>
+              <div style={{fontSize:"1rem", color: "white", marginTop: ".5rem"}}>{artist.name}</div>
+              <div className="text-muted">{artist.genres}</div>
+            </div>
+          </div>
+          ))}
+          </div>
+      </div>
+      </div>
   )
 }
