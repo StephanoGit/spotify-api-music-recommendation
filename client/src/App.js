f7bd38d2ec86f131f316b77a7b1dc7e78bd93c74
Radu-Stefan Popovici <radu.popovici-2@student.manchester.ac.uk>
2023-07-06 16:47:30 +0300

    Navigation bar
    A new navigation has been added that makes it easier for the user to navigate the website

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./style/App.css";
 
 import React, { useEffect, useState } from 'react'
-import { Container } from "react-bootstrap"
 import SpotifyWebApi from "spotify-web-api-js"
 import Marquee from "react-fast-marquee"
 
@@ -10,14 +9,12 @@ import Login from "./Login"
 import Dashboard from "./Dashboard"
 import Profile from "./Profile"
 import Recommendations from "./Recommendations";
-// import PlaylistDashboard from "./PlaylistDashboard"
-
 
 const spotifyApi = new SpotifyWebApi()
 
 function App() {
     const [spotifyToken, setSpotifyToken] = useState(null)
-    const [state, setState] = useState('')
+    const [state, setState] = useState('profile')
 
     useEffect(() => {
         const hash = window.location.hash
@@ -34,7 +31,7 @@ function App() {
 
     return (
     <div>
-        <div className="banner" style={{position: "fixed", top: "0"}}>
+        <div className="banner" style={{position: "fixed", top: "0", zIndex: "999"}}>
             <Marquee>
                 <div className="banner-text text1">	
                     {[...Array(14)].map((e, i) => (<span key={i}>FIND YOUR FAVOURITE MUSIC</span>))}
@@ -44,20 +41,15 @@ function App() {
 
     {spotifyToken ? 
     <div className="app">
-        <Container className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: "90vh" }}>
-            <Container className="d-flex flex-row justify-content-center align-items-center" style={{ minHeight: "70vh" }}>
-                <div className="oswald-font" style={{fontSize:"86px"}}>
-                    SPOTIFY MUSIC RECOMMENDATION
-                </div>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" width={"400px"}/>
-            </Container>
-
-            <Container className="d-flex flex-row justify-content-around align-items-center">
-                <a className="btn btn-lg rounded-pill" style={{backgroundColor: "#212121", color: "white"}} onClick={() => setState('profile')}>See Profile</a>
-                <a className="btn btn-lg rounded-pill" style={{backgroundColor: "#1ED760", color: "white", fontSize: "28px"}} onClick={() => setState('recommendation')}>Get Recommendations</a>
-                <a className="btn btn-lg rounded-pill" style={{backgroundColor: "#212121", color: "white"}} onClick={() => setState('tracks')}>Serach Tracks</a>
-            </Container>
-        </Container>
+        <div className="navbar">
+            <div className="d-flex justify-content-end" style={{width: "100%", gap: "2rem"}}>
+                <a href="https://open.spotify.com/" target="_blank">SPOTIFY</a>
+                <a onClick={() => setState('profile')}>PROFILE</a>
+                <a onClick={() => setState('recommendation')}>RECOMMENDATION</a>
+                <a onClick={() => setState('tracks')}>SEARCH</a>
+            </div>
+            <div className="separator"></div>
+        </div>
 
         <div>
             {state ==='recommendation' && <Recommendations spotifyApi={spotifyApi}/>}
@@ -67,7 +59,7 @@ function App() {
         </div>
     </div> : <Login />}
 
-        <div className="banner" style={{position: "fixed", bottom: "0"}}>
+        <div className="banner" style={{position: "fixed", bottom: "0", zIndex: "999"}}>
             <Marquee>
                 <div className="banner-text text1">	
                     {[...Array(14)].map((e, i) => (<span key={i}>FIND YOUR FAVOURITE MUSIC</span>))}
